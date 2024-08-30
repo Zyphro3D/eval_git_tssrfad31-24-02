3e901a224c76bc95298a8292cbccc6f8afb4b760
Zyphro3D <zyphro3D@gmail.com>
2024-08-30 08:58:31 +0200

    modif du script pour un truc qui ma trotté dans la tête toute la nuit ^^

--- systeme/script.sh
+++ systeme/script.sh
@@ -5,7 +5,7 @@ echo "Ou voulez-vous enregistrer le projet ?"
 echo "(exemple : C:/dossier)"
 
 #récupéré l'emplacement racide
-read directory
+read -r directory
 
 #Afficher dans le terminal : Quel est le nom de votre projet ? 
 echo "Quel est le nom de votre projet ?"
@@ -21,7 +21,7 @@ else
     read create_dir
 
     if [ "$create_dir" = "oui" ]; then
-        mkdir "$directory"
+        mkdir -p "$directory"
         echo "Le dossier $directory a été créé."
         cd "$directory" || { echo "impossible de se déplacer dans le dossier $directory."; exit 1; }
     else
@@ -29,6 +29,41 @@ else
         exit 1
     fi
 fi
+
+# Vérifier si le dossier du projet existe déja
+if [ -d "$project" ]; then
+    echo "Le projet $project existe déjà. Que voulez-vous faire ?"
+    echo "1) Remplacer le projet"
+    echo "2) Changer de nom"
+    echo "3) Annuler l'opération"
+    read choice
+
+    case $choice in
+        1)
+            rm -rf "$project"
+            echo "Le dossier $project a été remplacé."
+            ;;
+        2)
+            echo "Veuillez entrer un nouveau nom pour le projet :"
+            read new_project
+            project=$new_project
+            while [ -d "$project" ]; do
+                echo "Le projet $project existe déjà. Veuillez choisir un autre nom :"
+                read project
+            done
+            ;;
+        3)
+            echo "Opération annulée."
+            exit 1
+            ;;
+        *)
+            echo "Choix annulé. Opération annulée."
+            exit 1
+            ;;
+    esac
+fi
+
+
 
 #Créer le dossier du projet
 mkdir $project
